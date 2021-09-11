19bf20debf51b72934090570dcfeba3a3d4b9410
John M. Stassen <john.stassen@gmail.com>
2021-09-11 14:25:31 -0700

    commit tree 19e7c8dd6d1dad5c3bf52b57645711713d87b0a4

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!!this.state.username && !!this.state.password) {
+      return this.props.handleLogin(this.state)
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleChange} id="username" name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handleChange} id="password" name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleMsgChange = (e) => {
+    this.setState({
+      message: e.target.value
+    })
   }
 
   render() {
+    let characters = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <p>Characters Remaining: {characters}</p>
+        <input onChange={this.handleMsgChange} type="text" name="message" id="message" value={this.state.message}/>
       </div>
     );
   }
